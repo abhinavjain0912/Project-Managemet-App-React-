098b6438057e4de82fde5347d3bbc7e76d9ff6fe
Abhinav Jain <abhinavjain2233@gmail.com>
2024-05-20 13:34:10 +0530

    Adding No Project Selected Component and Adding a reusable Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+// SelectedProject Property will be used to store the id of the project if we have multiple projects 
+// null if no project is selected and we want to create a new project
+// and undefined if we are not adding a new project and neither selecting a project
+
+import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import ProjectsSidebar from './components/ProjectsSideBar.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState((prevState)=>{
+      return {
+        ...prevState,
+        selectedProjectId : null,
+      }
+    })
+  }
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
